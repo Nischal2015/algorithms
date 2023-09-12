8b63e46cb173c95dee2454c4de75c3be14f41836
Nischal2015 <nischalshakya2015@gmail.com>
2023-09-12 14:57:20 +0545

    Implement binary tree diameter algorithm

---
+++ binaryTreeDiameter.py
@@ -0,0 +1,27 @@
+class BinaryTree:
+  def __init__(self, value, right=None, left=None):
+    self.value = value
+    self.right = right
+    self.left = left
+
+def binaryTreeDiameter(tree):
+  return getTreeInfo(tree).diameter
+
+def getTreeInfo(tree):
+  if tree is None:
+    return TreeInfo(0, 0)
+
+  leftTreeInfo = getTreeInfo(tree.left)
+  rightTreeInfo = getTreeInfo(tree.right)
+
+  longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height
+  maxDiameterSoFar = max(leftTreeInfo.diameter, rightTreeInfo.diameter)
+  currentDiameter = max(longestPathThroughRoot, maxDiameterSoFar)
+  currentHeight = 1 + max(leftTreeInfo.height, rightTreeInfo.height)
+
+  return TreeInfo(currentDiameter, currentHeight)
+
+class TreeInfo:
+  def __init__(self, diameter, height):
+    self.diameter = diameter
+    self.height = height
