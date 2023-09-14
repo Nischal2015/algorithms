01c0021e57180e1967f2716bfb189aa0d89e1b7f
Nischal2015 <nischalshakya2015@gmail.com>
2023-09-14 11:19:10 +0545

    Implement branch sum algorithm

---
+++ algorithms/branchSums.py
@@ -0,0 +1,26 @@
+class BranchSums:
+    """
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    """
+
+    def __init__(self, node):
+        self.node = node
+        self.sumList = []
+
+    def calculateSums(self):
+        self.__sum(self.node, 0, self.sumList)
+
+        return self.sumList
+
+    def __sum(self, node, runningSum, sumList):
+        if node is None:
+            return
+
+        newRunningSum = runningSum + node.value
+        if node.left is None and node.right is None:
+            sumList.append(newRunningSum)
+            return
+
+        self.__sum(node.left, newRunningSum, sumList)
+        self.__sum(node.right, newRunningSum, sumList)
