f42fa2c1569fa60f1d9d3bb803a3c52276d19d05
Nischal2015 <nischalshakya2015@gmail.com>
2023-09-14 11:17:41 +0545

    Refactor to use BST module for better organization

--- algorithms/findClosestValueinBST.py
+++ algorithms/findClosestValueinBST.py
@@ -1,36 +1,12 @@
-class BSTNode:
-    def __init__(self, value):
-        self.value = value
-        self.right = None
-        self.left = None
-
-
-class BinarySearchTree:
-    def __init__(self, array):
-        self.array = array
-        self.tree = None
-
-    def getBST(self):
-        self.__constructBST()
-
-        return self.tree
-
-    def insert(self, tree, value):
-        if tree is None:
-            return BSTNode(value)
-
-        if value < tree.value:
-            tree.left = self.insert(tree.left, value)
-        else:
-            tree.right = self.insert(tree.right, value)
-        return tree
-
-    def __constructBST(self):
-        for i in self.array:
-            self.tree = self.insert(self.tree, i)
+from BST.binarySearchTree import BinarySearchTree
 
 
 class FindClosestValueInBST:
+    """
+    Time Complexity: O(log(n))
+    Space Complexity: O(log(n))
+    """
+
     def __init__(self, binaryTree):
         self.binaryTree = binaryTree
         self.closestDistance = float('inf')
