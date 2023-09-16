8195c3eb265d4177f97757e638e170fb2c1a5be5
Nischal Shakya <nischalshakya2015@gmail.com>
2023-09-16 21:33:37 +0545

    Implement class photo algorithm

---
+++ algorithms/classPhotos.py
@@ -0,0 +1,29 @@
+class ClassPhotos:
+  """
+  Time Complexity: O(n*log(n))
+  Space Complexity: O(n)
+  """
+
+  def __init__(self, redShirtList, blueShirtList):
+    self.redShirtList = redShirtList
+    self.blueShirtList = blueShirtList
+
+  def canArrangeClassPhoto(self):
+    redShirtSorted = sorted(self.redShirtList, reverse=True)
+    blueShirtSorted = sorted(self.blueShirtList, reverse=True)
+
+    redShirtHasMaxHeight = redShirtSorted[0] > blueShirtSorted[0]
+
+    for i in range(len(redShirtSorted)):
+      if redShirtHasMaxHeight and blueShirtSorted[i] > redShirtSorted[i]:
+        return False
+      elif not redShirtHasMaxHeight and redShirtSorted[i] > blueShirtSorted[i]:
+        return False
+      
+    return True
+
+redShirt = [5,8,1,3,4]
+blueShirt = [6,9,2,4,5]
+
+classPhotos = ClassPhotos(redShirt, blueShirt)
+print(classPhotos.canArrangeClassPhoto())
