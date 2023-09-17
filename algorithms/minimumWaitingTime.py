2cc3abf2a702f8364e37302bd76b3824a5c48af0
Nischal Shakya <nischalshakya2015@gmail.com>
2023-09-17 08:43:40 +0545

    Format code with PEP-8 standards

--- algorithms/minimumWaitingTime.py
+++ algorithms/minimumWaitingTime.py
@@ -1,20 +1,21 @@
 class MinimumWaitingTime:
-  """
-  Time Complexity: O(n*log(n))
-  Space Complexity: O(n)
-  """
+    """
+    Time Complexity: O(n*log(n))
+    Space Complexity: O(n)
+    """
 
-  def __init__(self, timeArray):
-    self.timeArray = timeArray
+    def __init__(self, timeArray):
+        self.timeArray = timeArray
 
-  def calculateTime(self):
-    sortedTimeArray = sorted(self.timeArray)
-    minimumWaitingTime = 0
+    def calculateTime(self):
+        sortedTimeArray = sorted(self.timeArray)
+        minimumWaitingTime = 0
 
-    for index, num in enumerate(reversed(sortedTimeArray)):
-      minimumWaitingTime += index * num
+        for index, num in enumerate(reversed(sortedTimeArray)):
+            minimumWaitingTime += index * num
 
-    return minimumWaitingTime
+        return minimumWaitingTime
 
-waitingTime = MinimumWaitingTime([3,2,1,2,6])
+
+waitingTime = MinimumWaitingTime([3, 2, 1, 2, 6])
 print(waitingTime.calculateTime())
--- algorithms/tandemBicycle.py
+++ algorithms/tandemBicycle.py
@@ -1,25 +1,28 @@
 class TandemBicycle:
-  """
-  Time Complexity: O(n*log(n))
-  Space Complexity: O(n)
-  """
+    """
+    Time Complexity: O(n*log(n))
+    Space Complexity: O(n)
+    """
 
-  def __init__(self, redShirtSpeeds, blueShirtSpeeds, fastest):
-    self.redShirtSpeeds = redShirtSpeeds
-    self.blueShirtSpeeds = blueShirtSpeeds
-    self.fastest = fastest
+    def __init__(self, redShirtSpeeds, blueShirtSpeeds, fastest):
+        self.redShirtSpeeds = redShirtSpeeds
+        self.blueShirtSpeeds = blueShirtSpeeds
+        self.fastest = fastest
 
-  def tandemBicycleSpeed(self):
-    totalScore = 0
+    def tandemBicycleSpeed(self):
+        totalScore = 0
 
-    redShirtSpeedsSorted = sorted(self.redShirtSpeeds)
-    blueShirtSpeedsSorted = sorted(self.blueShirtSpeeds, reverse=self.fastest)
+        redShirtSpeedsSorted = sorted(self.redShirtSpeeds)
+        blueShirtSpeedsSorted = sorted(
+            self.blueShirtSpeeds, reverse=self.fastest)
 
-    for i in range(len(redShirtSpeedsSorted)):
-      totalScore += max(redShirtSpeedsSorted[i], blueShirtSpeedsSorted[i])
+        for i in range(len(redShirtSpeedsSorted)):
+            totalScore += max(redShirtSpeedsSorted[i],
+                              blueShirtSpeedsSorted[i])
 
-    return totalScore
-  
+        return totalScore
+
+
 redShirtSpeeds = [5, 5, 3, 9, 2]
 blueShirtSpeeds = [3, 6, 7, 2, 1]
 tandemBicycle = TandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest=False)
