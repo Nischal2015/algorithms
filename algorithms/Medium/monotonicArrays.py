bfd3c3b64a3c0ef6f3da1751252275eee12d3558
Nischal2015 <nischalshakya2015@gmail.com>
2023-09-20 22:40:12 +0545

    Implement moveToEnd and monotonicArrays algorithm

---
+++ algorithms/Medium/monotonicArrays.py
@@ -0,0 +1,24 @@
+class MonotonicArrays:
+    def isMonotonic(self, array):
+        arrayLength = len(array)
+
+        if arrayLength <= 2:
+            return True
+
+        firstIdx = 1
+        while array[firstIdx-1] == array[firstIdx]:
+            firstIdx += 1
+            if firstIdx == arrayLength:
+                return True
+
+        direction = 1 if array[firstIdx-1] < array[firstIdx] else -1
+        while firstIdx < arrayLength:
+            if direction * array[firstIdx-1] > direction * array[firstIdx]:
+                return False
+            firstIdx += 1
+        return True
+
+
+monotonicArrays = MonotonicArrays()
+print(monotonicArrays.isMonotonic(
+    [-1, -1, -1, -2, -3, -4]))
---
+++ algorithms/Medium/moveElementToEnd.py
@@ -0,0 +1,45 @@
+import time
+
+from more_itertools import first
+
+
+class MoveElementToEnd:
+    def __init__(self):
+        pass
+
+    def move(self, array, target):
+        firstIdx = 0
+        lastIdx = len(array)-1
+
+        while firstIdx < lastIdx:
+            while array[lastIdx] == target:
+                lastIdx -= 1
+
+            while array[firstIdx] != target:
+                firstIdx += 1
+
+            if firstIdx < lastIdx:
+                array[firstIdx], array[lastIdx] = array[lastIdx], array[firstIdx]
+
+            firstIdx += 1
+
+        return array
+
+    # def move(self, array, target):
+    #     lastIdx = len(array)-1
+
+    #     while firstIdx < lastIdx:
+    #         while firstIdx < lastIdx and array[lastIdx] == target:
+    #             lastIdx -= 1
+
+    #         if array[firstIdx] == target:
+    #             array[firstIdx], array[lastIdx] = array[lastIdx], array[firstIdx]
+
+    #         firstIdx += 1
+
+    #     return array
+
+
+moveElementToEnd = MoveElementToEnd()
+print(moveElementToEnd.move(
+    [2, 2, 4, 5, 6, 23, 2, 2, 2, 2, 3, 4, 2], 2))
